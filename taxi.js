23ac7d8aca05c7185adfb7546e53d386f079ddcd
Anna Casper <anna.casper3@gmail.com>
2015-07-14 14:56:56 -0600

    all taxi specs are passing

--- taxi.js
+++ taxi.js
@@ -13,5 +13,17 @@ Taxi.prototype.passengerCount = function(){
   return this.passengers.length;
 }
 
+Taxi.prototype.full = function(){
+  if(this.passengers.length >= this.maxNumberOfPassengers){
+    return true
+  } else {
+    return false
+  }
+}
+
+Taxi.prototype.dropOffPassengers = function(){
+  this.passengers = [];
+}
+
 // DO NOT MODIFY BELOW THIS COMMENT:
 module.exports = Taxi;
